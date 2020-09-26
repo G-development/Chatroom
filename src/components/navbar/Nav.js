55448ba26c1721bf675dccd6a26b5133d0f2313a
Horizons-code <cuozzogiovanni03@gmail.com>
2020-09-26 12:40:53 +0200

    Initial commit

---
+++ src/components/navbar/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css'
+
+function Nav(){
+    return(
+        <nav>
+            <ul>
+                <li><a href="#">Logo</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Link1</a></li>
+                <li><a href="#">Link2</a></li>
+                <li><a href="#">Link3</a></li>
+                <li><a href="#">Link4</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav 
